85fac27a978c745e51ed82d1b8af00936b0e8676
Francisco Martins <francisco.mtins@gmail.com>
2024-11-23 19:41:05 -0300

    Refatorando TCs para implementação modelo Table

--- features/catalogo.feature
+++ features/catalogo.feature
@@ -10,20 +10,52 @@ Cenário: Acessar o catálogo de cafés na página principal
     Então eu devo ver uma lista de cafés disponíveis
 
 
-Cenário:  Iniciar a compra de um café
+# Cenário:  Iniciar a compra de um café
+
+#     Dado que estou na página principal da Starbugs
+#        E que desejo comprar o café "Expresso Gelado"
+#        E que esse produto custa "R$ 9,99"
+#        E que o custo de entrega é de "R$ 10,00"  
+#     Quando inicio a compra desse item 
+#     Então devo ver a página de checkout com os detalhes do produto
+#        E o valor total da compra deve ser de "R$ 19,99"
+
+# Cenário:  Iniciar a compra de um café - DataTable Modelo para N
+
+#     Dado que estou na página principal da Starbugs
+#        E que desejo comprar o seguinte produto:
+#        | product         | price   | delivery |
+#        | Expresso Gelado | R$ 9,99 | R$ 10,00 |
+#     Quando inicio a compra desse item 
+#     Então devo ver a página de checkout com os detalhes do produto
+#        E o valor total da compra deve ser de "R$ 19,99"
+
+
+@temp
+Cenário:  Iniciar a compra de um café - DataTable Model para 1
 
     Dado que estou na página principal da Starbugs
-       E que desejo comprar o café "Expresso Gelado"
-       E que esse produto custa "R$ 9,99"
-       E que o custo de entrega é de "R$ 10,00"  
+       E que desejo comprar o seguinte produto:
+       | name     | Expresso Gelado |
+       | price    | R$ 9,99         |
+       | delivery | R$ 10,00        |
     Quando inicio a compra desse item 
     Então devo ver a página de checkout com os detalhes do produto
        E o valor total da compra deve ser de "R$ 19,99"
 
-@temp
+
+# Cenário: Café indisponível 
+
+#     Dado que estou na página principal da Starbugs
+#        E que desejo comprar o café "Expresso Cremoso" 
+#     Quando inicio a compra desse item
+#     Então devo ver um popup informando que o produto está indisponível
+
+
 Cenário: Café indisponível 
 
     Dado que estou na página principal da Starbugs
-       E que desejo comprar o café "Expresso Cremoso" 
+       E que desejo comprar o seguinte produto:
+       | name     | Expresso Cremoso |
     Quando inicio a compra desse item
     Então devo ver um popup informando que o produto está indisponível
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -11,29 +11,51 @@ Dado('que estou na página principal da Starbugs') do
     visit 'https://starbugs.vercel.app/'
  end
 
-Dado('que desejo comprar o café {string}') do |product_name|
-    @product_name = product_name
-end
+## funões para o modelo padrão de validação de Iniciar a compra de um café - modelo Table N
+## Para usar no modelo tradicional e Table N, a variável deve ser substituida para validação unitária
 
-Dado('que esse produto custa {string}') do |product_price|
-    @product_price = product_price
-end
+#  Dado('que desejo comprar o seguinte produto:') do |table|
+#     @product_name = table.hashes[0][:product]
+#     @product_price = table.hashes[0][:price]
+#     @delivery_price = table.hashes[0][:delivery]   
+# end
+##########################################
 
-Dado('que o custo de entrega é de {string}') do |delivery_price|
-    @delivery_price = delivery_price
+## funões para o modelo padrão de validação de Iniciar a compra de um café
+# Dado('que desejo comprar o café {string}') do |product_name|
+#     @product_name = product_name
+# end
+
+# Dado('que esse produto custa {string}') do |product_price|
+#     @product_price = product_price
+# end
+
+# Dado('que o custo de entrega é de {string}') do |delivery_price|
+#     @delivery_price = delivery_price
+# end
+##########################################
+
+# funões para o modelo padrão de validação de Iniciar a compra de um café - modelo Table N
+
+ Dado('que desejo comprar o seguinte produto:') do |table|
+  @product = table.rows_hash  
 end
+#####
 
 Quando('inicio a compra desse item') do
-    product = find('.coffee-item', text: @product_name)
+    product = find('.coffee-item', text: @product[:name])
     product.find('.buy-coffee').click 
 end
 
 Então('devo ver a página de checkout com os detalhes do produto') do
     product_title = find('.item-details h1')
-    expect(product_title.text).to eql @product_name
+    expect(product_title.text).to eql @product[:name]
 
     sub_price = find('.subtotal .sub-price')
-    expect(sub_price.text).to eql @product_price
+    expect(sub_price.text).to eql @product[:price]
+
+    delivery = find('.delivery-price')
+    expect(delivery.text).to eql @product[:delivery]
 end
 
 Então('o valor total da compra deve ser de {string}') do |total_price|
@@ -45,3 +67,4 @@ Então('devo ver um popup informando que o produto está indisponível') do
     popup = find('.swal2-html-container')
     expect(popup.text).to eql 'Produto indisponível'
 end
+
